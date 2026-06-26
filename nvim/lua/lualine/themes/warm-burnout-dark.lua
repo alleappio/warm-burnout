c21f21c3abe39bc4fd6c006598b20f9f71972fea
alleappio <alleappio@gmail.com>
2026-06-26 10:34:17 +0200

    add dark burnout theme for lualine
    Signed-off-by: alleappio <alleappio@gmail.com>

---
+++ nvim/lua/lualine/themes/warm-burnout-dark.lua
@@ -0,0 +1,37 @@
+local palette = require('warm-burnout.palette').dark
+local theme
+
+theme = {
+    normal = {
+        a = { bg = palette.accent, fg = palette.bg, gui = 'bold' },
+        b = { bg = palette.bg_highlight, fg = palette.fg },
+        c = { bg = palette.bg, fg = palette.fg },
+    },
+    insert = {
+        a = { bg = palette.string, fg = palette.bg, gui = 'bold' },
+        b = { bg = palette.bg_highlight, fg = palette.fg },
+        c = { bg = palette.bg, fg = palette.fg },
+    },
+    visual = {
+        a = { bg = palette.func, fg = palette.bg, gui = 'bold' },
+        b = { bg = palette.bg_highlight, fg = palette.fg },
+        c = { bg = palette.bg, fg = palette.fg },
+    },
+    replace = {
+        a = { bg = palette.type, fg = palette.bg, gui = 'bold' },
+        b = { bg = palette.bg_highlight, fg = palette.fg },
+        c = { bg = palette.bg, fg = palette.fg },
+    },
+    command = {
+        a = { bg = palette.constant, fg = palette.bg, gui = 'bold' },
+        b = { bg = palette.bg_highlight, fg = palette.fg },
+        c = { bg = palette.bg, fg = palette.fg },
+    },
+    inactive = {
+        a = { bg = palette.bg, fg = palette.comment, gui = 'bold' },
+        b = { bg = palette.bg_highlight, fg = palette.fg },
+        c = { bg = palette.bg, fg = palette.fg },
+    },
+}
+
+return theme
